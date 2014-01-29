96b97d1e4342dd4000b874b286f265367b952377
Sunny Juneja <me@sunnyjunjea.com>
2014-01-28 17:49:26 -0800

    Complete API wrapper and basic specs

--- lib/open_civic_data/client.rb
+++ lib/open_civic_data/client.rb
@@ -14,7 +14,31 @@ module OpenCivicData
     end
 
     def jurisdictions(options = {})
-      get('/jurisdictions', options)
+      get('/jurisdictions/', options)
+    end
+
+    def divisions(options = {})
+      get('/divisions/', options)
+    end
+
+    def people(options = {})
+      get('/people/', options)
+    end
+
+    def organizations(options = {})
+      get('/organizations/', options)
+    end
+
+    def events(options = {})
+      get('/events/', options)
+    end
+
+    def bills(options = {})
+      get('/bills/', options)
+    end
+
+    def votes(options = {})
+      get('/votes/', options)
     end
   end
 end
