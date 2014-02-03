183f8a4dbd9d66dedcd08dd504630e46a0078f3f
Sunny Juneja <me@sunnyjunjea.com>
2014-02-02 16:18:56 -0800

    Add examples to README. Improve documentation for yard. Improve yardstick coverage.

--- lib/open_civic_data/client.rb
+++ lib/open_civic_data/client.rb
@@ -13,79 +13,72 @@ module OpenCivicData
       @key = key
     end
 
-    # Jurisdictions are governing bodies that exists within a division.
-    # Jurisdiction's name, url, chambers, terms, and session details.
-    #
-    # return [Hashie::Rash]
+    # Fetches governing bodies that exist within a division
+    # @return [Hashie::Rash] Jurisdiction's name, url, chambers, terms, and session details.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.jurisdictions
+    # @api public
     def jurisdictions(options = {})
       get('/jurisdictions/', options)
     end
 
-    # Divisions are political geographies such as a state, county, or congressional district.
-    # Division's geometries and list of child jurisdiction ids.
-    #
-    # @return [Hashie::Rash]
+    # Fetches political geographies such as a state, county or congressional district
+    # @return [Hashie::Rash] Division's geometries and list of child jurisdiction ids.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.divisions
+    # @api public
     def divisions(options = {})
       get('/divisions/', options)
     end
 
-    # People, typically a politician or gov official.
-    # Person's name, image, contact details, and links.
-    #
-    # @return [Hashie::Rash]
+    # Fetches people, typically politicians or gov officials
+    # @return [Hashie::Rash] Person's name, image, contact details, and links.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.people
+    # @api public
     def people(options = {})
       get('/people/', options)
     end
 
-    # A group of people, such as a city council, state senate, or committee.
-    # Organization's name, classification, parent id, contact details, links and posts.
-    #
-    # @return [Hashie::Rash]
+    # Fetches groups of people, such as a city council, state senate or committee
+    # @return [Hashie::Rash] Organization's name, classification, parent id, contact details, links and posts.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.organizations
+    # @api public
     def organizations(options = {})
       get('/organizations/', options)
     end
 
-    # A legislative event, such as a meeting or hearing.
-    # Event's type, name, description, when, end, status, location, and linked entities.
-    #
-    # @return [Hashie::Rash]
+    # Fetches legislative event, such as a meeting or hearing
+    # @return [Hashie::Rash] Event's type, name, description, when, end, status, location, and linked entities.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.events
+    # @api public
     def events(options = {})
       get('/events/', options)
     end
 
-    # Legislative documents, resolutions, appointments or contract and its history,
-    # Bill's type, legislative body, session, name, chamber, title, type, subject and summary.
-    #
-    # @return [Hashie::Rash]
+    # Fetches legislative documents and their history
+    # @return [Hashie::Rash] Bill's type, legislative body, session, name, chamber, title, type, subject and summary.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.bills
+    # @api public
     def bills(options = {})
       get('/bills/', options)
     end
 
-    # The record of a vote taken on a motion. May contain individual legilsator's yay/nay or outcome.
-    # Vote's organization, session, chamber, date, motion, type and passed or not.
-    #
-    # @return [Hashie::Rash]
+    # Fetches the record of vote taken on motions
+    # @return [Hashie::Rash] Vote's organization, session, chamber, date, motion, type and passed or not.
     # @example
     #   OpenCivicData.key = YOUR_SUNLIGHT_API_KEY
     #   OpenCivicData.votes
+    # @api public
     def votes(options = {})
       get('/votes/', options)
     end
